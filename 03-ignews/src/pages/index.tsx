8c28f49ed24c64bf8cd2f9575caa561a11eecace
Leonardo Maron <leo22.maron@hotmail.com>
2022-05-10 23:50:03 -0300

    03 - Static Site Generation (SSG)

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { stripe } from '../services/stripe';
@@ -6,6 +6,10 @@ import { stripe } from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
+
+// Client-side - useEffect
+// Server-side - GetServerSideProps
+// Static Site Generation - GetStaticProps
 
 interface HomeProps {
   product: {
@@ -42,7 +46,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JzS26LeDgFg2oTs1FmZRQgs');
 
   const product = {
@@ -56,6 +60,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
