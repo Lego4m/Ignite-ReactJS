110b07d35bd9813c86100e1c83956e895bb4d902
Leonardo Maron <leo22.maron@hotmail.com>
2022-05-13 23:46:12 -0300

    03 - Redirecionando para o Stripe

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe.js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,7 +11,7 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github');
       return;
@@ -16,7 +19,17 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
     // Criação da checkout session
 
-    
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    } 
   }
 
   return (
