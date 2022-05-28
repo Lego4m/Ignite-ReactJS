92ee35341ae6d239efb793e102f27ddbbacf2d0f
Leonardo Maron <leo22.maron@hotmail.com>
2022-05-27 21:59:36 -0300

    04 - Listagem responsiva

--- 04-dashgo/src/components/Pagination/index.tsx
+++ 04-dashgo/src/components/Pagination/index.tsx
@@ -1,10 +1,10 @@
-import { Box, HStack } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
 import { PaginationItem } from './PaginationItem';
 
 export function Pagination() {
   return (
-    <HStack
-      direction='row'
+    <Stack
+      direction={['column', 'row']}
       spacing='6'
       mt='8'
       justify='space-between'
@@ -14,12 +14,12 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       
-      <HStack direction='row' spacing='2'>
+      <Stack direction='row' spacing='2'>
         <PaginationItem number={1} isCurrent />
         <PaginationItem number={2} />
         <PaginationItem number={3} />
         <PaginationItem number={4} />
-      </HStack>
-    </HStack>
+      </Stack>
+    </Stack>
   )
 }
--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -11,7 +11,8 @@ import {
   Th, 
   Thead, 
   Tr,
-  Text
+  Text,
+  useBreakpointValue
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
@@ -20,6 +21,11 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const isWideVesrion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -45,18 +51,18 @@ export default function UserList() {
           <Table colorScheme='whiteAlpha'>
             <Thead>
               <Tr>
-                <Th px='6' color='gray.300' width='8'>
+                <Th px={['4', '4', '6']} color='gray.300' width='8'>
                   <Checkbox colorScheme='pink'/>
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                { isWideVesrion && <Th>Data de cadastro</Th> }
                 <Th width='8'></Th>
               </Tr>
             </Thead>
 
             <Tbody>
               <Tr>
-                <Td px='6'>
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme='pink'/>
                 </Td>
                 <Td>
@@ -67,7 +73,7 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                { isWideVesrion && <Td>04 de Abril, 2021</Td> }
                 <Td>
                   <Button 
                     as='a' 
@@ -76,7 +82,7 @@ export default function UserList() {
                     colorScheme='purple'
                     leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
                   >
-                    Editar
+                    { isWideVesrion && 'Editar' }
                   </Button>
                 </Td>
               </Tr>
