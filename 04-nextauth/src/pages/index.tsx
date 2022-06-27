09cd87b1196c212de51b9eab387031ffb10cc422
Leonardo Maron <leo22.maron@hotmail.com>
2022-06-26 22:12:01 -0300

    04 - Validando visitante * highorder function

--- 04-nextauth/src/pages/index.tsx
+++ 04-nextauth/src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
-import { parseCookies } from 'nookies';
 
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const { signIn } = useContext(AuthContext);
@@ -31,19 +31,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async (ctx) => {
   return {
-    props: {},
+    props: {}
   }
-}
+})
