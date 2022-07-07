cd93e1de657bc26ce090568cb9409e5154e401de
Leonardo Maron <leo22.maron@hotmail.com>
2022-07-06 23:09:21 -0300

    05 - useCallback * Igualdade referencial

--- 05-perfreact/src/components/ProductItem.tsx
+++ 05-perfreact/src/components/ProductItem.tsx
@@ -6,6 +6,7 @@ interface ProductItemProps {
     price: number;
     title: string;
   }
+  onAddToWishList: (id: number) => void;
 }
 
 // shallow compare -> comparação rasa
@@ -13,10 +14,11 @@ interface ProductItemProps {
 // {} === {} // false
 // igualdade referencial
 
-function ProductItemComponent({ product }: ProductItemProps) {
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
+      <button onClick={() => onAddToWishList(product.id)}>Add to wishlist</button>
     </div>
   )
 }
