1644cf540da85f91814d6a05b196e338adc9fab8
Leonardo Maron <leo22.maron@hotmail.com>
2022-07-06 22:56:28 -0300

    05 - useMemo

--- 05-perfreact/src/components/SearchResults.tsx
+++ 05-perfreact/src/components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { ProductItem } from "./ProductItem";
+import { useMemo } from 'react';
+import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
   results: Array<{
@@ -9,13 +10,45 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0)
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map((product) => {
         return (
-          <ProductItem product={product} />
+          <ProductItem key={product.id} product={product} />
         )
       })}
     </div>
   )
 }
+
+/**
+ * Ciclo de renderização do React
+ * 
+ * 1. Cria uma nova versão do componente
+ * 2. Compara com a versão anterior
+ * 3. Se houver alterações, atualiza o que alterou
+ */
+
+/**
+ * memo
+ * 
+ * 1. Pure Functional Components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
+
+/** 
+ * useMemo / useCallback
+ * 
+ * 1. Cálculos pesados
+ * 2. Igualdade referencial (quando a gente repassa aquela informação a um componente filho)
+*/
