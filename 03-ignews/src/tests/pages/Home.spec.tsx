4c0a2a0e4258cc9e44b9410659639ca030212cba
Leonardo Maron <leo22.maron@hotmail.com>
2022-07-12 23:04:17 -0300

    05 - Testando getStaticProps

--- 03-ignews/src/tests/pages/Home.spec.tsx
+++ 03-ignews/src/tests/pages/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages';
+import { stripe } from '../../services/stripe';
+import Home, { getStaticProps } from '../../pages';
 
 jest.mock('next/router');
 jest.mock('next-auth/react', () => {
@@ -7,11 +8,35 @@ jest.mock('next-auth/react', () => {
     useSession: () => ({ data: null })
   }
 });
+jest.mock('../../services/stripe',);
 
 describe(('Home page'), () => {
   it('renders correctly', () => {
     render(<Home product={{ priceId: 'fake-price-id', amount: 'R$10,00' }} />)
   
     expect(screen.getByText('for R$10,00 month')).toBeInTheDocument();
+  });
+
+  it('loads initial data', async () => {
+    const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve);
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000,
+
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({ // Se o objeto contém, mas não precisa ser estritamente igual
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00',
+          }
+        }
+      })
+    )
   });
 });
